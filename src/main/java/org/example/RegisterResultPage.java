c0226219df4fc45a3101022d7dee72cfb88426d5
krishna0707patel <krishnapatel.0711@yahoo.com>
2022-11-16 22:06:45 +0000

    commit1

--- src/main/java/org/example/RegisterResultPage.java
+++ src/main/java/org/example/RegisterResultPage.java
@@ -10,6 +10,6 @@ public class RegisterResultPage extends Utils{
         waitForUrlToBe("https://demo.nopcommerce.com/registerresult/1?returnUrl=/", 20);
 
 
-         Assert.assertEquals(getTextFromElement(By.className("result")),"Your registration completed successfully ","UnSuccessfully registered");
+        Assert.assertEquals(getTextFromElement(By.className("result")),"Your registration completed successfully ","UnSuccessfully registered");
     }
 }
